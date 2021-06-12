7fe13c1e4ced59b3b05f91d6fca0ca98088c2603
Samuel Ezeala <sasmuelezeala@Sasmuels-MacBook-Pro.local>
2021-06-12 23:30:45 +0100

    auth completed

--- com.vreshly/wwwroot/js/jwtok.js
+++ com.vreshly/wwwroot/js/jwtok.js
@@ -51,16 +51,62 @@
         }
     }
 
-    var checkAuthStat = function () {
-        if (authItem != null) {
+    var checkAuthStat = function (_authItem) {
+        if (_authItem != null) {
             $('.login_register').css('display', 'none');
             $('.welcome_msg').css('display', 'block');
-            $('.welcome_msg').text(`Hello ${authItem.displayName}`)
+            $('.welcome_msg').text(`Hello ${_authItem.displayName}`)
         } else {
             $('.welcome_msg').css('display', 'none');
             $('.login_register').css('display', 'block');
         }
     }
 
-    checkAuthStat();
+    checkAuthStat(authItem);
+
+/** toggle login on checkout  **/
+    var checkoutLoginToggle = function (_authItem) {
+        if (_authItem != null) {
+            $('#billing-address-div').css('display', 'block');
+            $('#checkout-login-div').css('display','none')
+        } else {
+            $('#billing-address-div').css('display', 'none');
+            $('#checkout-login-div').css('display', 'block')
+        }
+    }
+
+    checkoutLoginToggle(authItem);
+
+    $('#login_btn_chkout').on('click', function (e) {
+        e.preventDefault();
+        var _email = $('#login_email_chkout').val();
+        var _password = $('#login_password_chkout').val()
+
+        if (isEmail(_email) && validateForm(_password, 'Password')) {
+            loginDto = {
+                email: _email,
+                password: _password
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: `${baseUrl}Account/Logon`,
+                data: JSON.stringify(loginDto),
+                contentType: "application/json;charset=utf-8",
+                traditional: true,
+            }).done(function (response) {
+                loggedIn.email = response.email;
+                loggedIn.displayName = response.displayName;
+                loggedIn.token = response.token;
+                localStorage.setItem('auth', JSON.stringify(loggedIn));
+                checkAuthStat(loggedIn);
+                checkoutLoginToggle(loggedIn)
+                //window.location.href = "/Home/Index";
+
+            })
+               .fail(function (data) {
+                    alert('Username or Password is incorrect');
+                });
+        }
+    });
 })
